ea89a9c6c0f1304073f8067b2b1b3ef8cca231be
Gustavo <127263149+devggustavo@users.noreply.github.com>
2024-11-07 23:52:43 -0400

    Correção navbar

--- sitebetel/resources/views/inicial.blade.php
+++ sitebetel/resources/views/inicial.blade.php
@@ -1,59 +1,7 @@
 @extends('layouts.master')
 @section('master')
 
-{{-- <style>
-    /* Define uma altura mais compacta para as imagens */
-    .carousel-item img {
-        height: 830px; /* Ajuste a altura conforme necessário */
-        object-fit: cover; /* Cobre o espaço definido sem distorcer a imagem */
-        width: 100%;
-    }
-    .carousel-caption {
-        bottom: 10px; /* Deixa a legenda mais próxima do rodapé da imagem */
-    }
-</style>
-<body>
-<div id="carouselExampleCaptions" class="carousel slide">
-    <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
-            aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
-            aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
-            aria-label="Slide 3"></button>
-    </div>
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="https://i.ibb.co/XpsNsmk/Culto-de-ora-o-2-Apresenta-o.jpg"  width="10" height="10" alt="First Slide">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>First Slide Label</h5>
-                <p>Some representative placeholder content for the first slide.</p>
-            </div>
-        </div>
-        <div class="carousel-item">
-            <img src="https://media.slidesgo.com/storage/32901776/0-.jpg" alt="Second Slide">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Second Slide Label</h5>
-                <p>Some representative placeholder content for the second slide.</p>
-            </div>
-        </div>
-        <div class="carousel-item">
-            <img src="https://image.slidesdocs.com/responsive-images/slides/pt/0-modelo-powerpoint-dinamico-bonito-desenho-animado-pequenos-animais-educacao-infantil-ensino_5f59a1573a__960_540.jpg" alt="Third Slide">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Third Slide Label</h5>
-                <p>Some representative placeholder content for the third slide.</p>
-            </div>
-        </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
-</div>
-</body> --}}
+<h1>Meu sitee</h1>
+
 
 @endsection
--- sitebetel/resources/views/layouts/navgation.blade.php
+++ sitebetel/resources/views/layouts/navgation.blade.php
@@ -1,69 +1,105 @@
-<link rel="stylesheet" href={{ asset('css/navbar.css') }}>
-
-<nav class="navbar navbar-expand-lg bg-secondary bg-gradient">
-    <div class="container-fluid px-2 gap-2">
+<nav class="navbar navbar-expand-lg bg-light" role="navigation" aria-label="Barra de Navegação Principal">
+    <div class="container px-2">
         <!-- Logo e Nome -->
-        <div class="d-flex align-items-center navbar-brand" style="min-width: 5rem;">
-            <img src="https://i.ibb.co/C5XR7cB/logo-betel.png" alt="Logo" width="55" height="55" class="me-2">
-            <a href="{{ ('/ibbb') }}" id="navbarText" class="text-white m-0" href="" style="text-decoration: none; font-size: 18px">IGREJA BATISTA BÍBLICA BETEL</a>
+        <div class="d-flex align-items-center navbar-brand" role="banner">
+            <img src="https://i.ibb.co/C5XR7cB/logo-betel.png" alt="Logo da Igreja Batista Bíblica Betel" width="55"
+                height="55" class="me-2">
+            <a href="{{ url('/ibbb') }}" id="navbarText" class="text-dark m-0"
+                style="text-decoration: none; font-size: 18px"
+                aria-label="Página inicial da Igreja Batista Bíblica Betel">IGREJA BATISTA BÍBLICA BETEL</a>
         </div>
 
         <!-- Botão para expandir a navbar em dispositivos móveis -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alternar navegação">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <!-- Menu de Navegação Centralizado -->
-        <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ url('/ibbb/sobre') }}">Sobre</a>
+        <!-- Menu de Navegação e Ícones de Redes Sociais -->
+        <div class="collapse navbar-collapse justify-content-between gap-5" id="navbarNavDropdown">
+            <!-- Menu de Navegação Centralizado -->
+            <ul class="navbar-nav mx-auto gap-3" role="menubar">
+                <li class="nav-item" role="none">
+                    <a class="nav-link active" aria-current="page" href="{{ url('/ibbb/sobre') }}"
+                        role="menuitem">Sobre</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ url('/ibbb/cultos') }}">Cultos</a>
+                <li class="nav-item" role="none">
+                    <a class="nav-link active" aria-current="page" href="{{ url('/ibbb/cultos') }}"
+                        role="menuitem">Cultos</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ url('/ibbb/eventos') }}">Eventos</a>
+                <li class="nav-item" role="none">
+                    <a class="nav-link active" aria-current="page" href="{{ url('/ibbb/eventos') }}"
+                        role="menuitem">Eventos</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ url('/ibbb/midias') }}">Midias</a>
+                <li class="nav-item" role="none">
+                    <a class="nav-link active" aria-current="page" href="{{ url('/ibbb/midias') }}"
+                        role="menuitem">Mídias</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ url('/ibbb/contribuicoes') }}">Dizimos e Ofertas</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ url('/ibbb/materiais') }}">Materiais</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ url('/ibbb/contato') }}">Contato</a>
-                </li>
-                <!-- Botão de Login dentro do dropdown para dispositivos móveis -->
-                <li class="d-lg-none">
-                    <a href="{{ url('/ibbb/login') }}" title="Login" name="btnlogin" class="dropdown-item text-primary">
-                        Login
-                    </a>
+                <li class="nav-item d-lg-none" role="none">
+                    <a href="{{ url('/ibbb/login') }}" title="Login" name="btnlogin" class="nav-link text-primary"
+                        role="menuitem">Login</a>
                 </li>
             </ul>
-        </div>
 
-        <!-- Botão de Login visível apenas em telas grandes -->
-        <div class="d-none d-lg-flex">
-            <a href="{{ url('/ibbb/login') }}" title="Login" name="btnlogin" class="btn btn-sm btn-primary d-flex shadow mt-1"
-                role="button" data-bs-toggle="tooltip" data-bs-placement="top">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#fff" 
-                    viewBox="0 0 256 256">
-                    <path 
-                        d="M230.92,212c-15.23-26.33-38.7-45.21-66.09-54.16a72,72,0,1,0-73.66,0C63.78,166.78,40.31,185.66,25.08,212a8,8,0,1,0,13.85,8c18.84-32.56,52.14-52,89.07-52s70.23,19.44,89.07,52a8,8,0,1,0,13.85-8ZM72,96a56,56,0,1,1,56,56A56.06,56.06,0,0,1,72,96Z">
-                    </path>
-                </svg>
-                <span class="ml-1">Login</span>
-            </a>
+            <!-- Ícones de Redes Sociais -->
+            <div class="d-flex align-items-center gap-3" role="complementary" aria-label="Ícones de Redes Sociais">
+                <a href="https://www.facebook.com/IBBBETEL" class="nav-link"
+                    aria-label="Página do Facebook da Igreja Batista Bíblica Betel">
+                    <!-- Ícone do Facebook -->
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#000000"
+                        viewBox="0 0 256 256">
+                        <path
+                            d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm8,191.63V152h24a8,8,0,0,0,0-16H136V112a16,16,0,0,1,16-16h16a8,8,0,0,0,0-16H152a32,32,0,0,0-32,32v24H96a8,8,0,0,0,0,16h24v63.63a88,88,0,1,1,16,0Z">
+                        </path>
+                    </svg>
+                </a>
+                <a href="https://www.instagram.com/igrejabetel80/" class="nav-link"
+                    aria-label="Página do Instagram da Igreja Batista Bíblica Betel">
+                    <!-- Ícone do Instagram -->
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#000000"
+                        viewBox="0 0 256 256">
+                        <path
+                            d="M128,80a48,48,0,1,0,48,48A48.05,48.05,0,0,0,128,80Zm0,80a32,32,0,1,1,32-32A32,32,0,0,1,128,160ZM176,24H80A56.06,56.06,0,0,0,24,80v96a56.06,56.06,0,0,0,56,56h96a56.06,56.06,0,0,0,56-56V80A56.06,56.06,0,0,0,176,24Zm40,152a40,40,0,0,1-40,40H80a40,40,0,0,1-40-40V80A40,40,0,0,1,80,40h96a40,40,0,0,1,40,40ZM192,76a12,12,0,1,1-12-12A12,12,0,0,1,192,76Z">
+                        </path>
+                    </svg>
+                </a>
+                <a href="https://www.youtube.com/" class="nav-link"
+                    aria-label="Canal do YouTube da Igreja Batista Bíblica Betel">
+                    <!-- Ícone do YouTube -->
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#000000"
+                        viewBox="0 0 256 256">
+                        <path
+                            d="M164.44,121.34l-48-32A8,8,0,0,0,104,96v64a8,8,0,0,0,12.44,6.66l48-32a8,8,0,0,0,0-13.32ZM120,145.05V111l25.58,17ZM234.33,69.52a24,24,0,0,0-14.49-16.4C185.56,39.88,131,40,128,40s-57.56-.12-91.84,13.12a24,24,0,0,0-14.49,16.4C19.08,79.5,16,97.74,16,128s3.08,48.5,5.67,58.48a24,24,0,0,0,14.49,16.41C69,215.56,120.4,216,127.34,216h1.32c6.94,0,58.37-.44,91.18-13.11a24,24,0,0,0,14.49-16.41c2.59-10,5.67-28.22,5.67-58.48S236.92,79.5,234.33,69.52Zm-15.49,113a8,8,0,0,1-4.77,5.49c-31.65,12.22-85.48,12-86,12H128c-.54,0-54.33.2-86-12a8,8,0,0,1-4.77-5.49C34.8,173.39,32,156.57,32,128s2.8-45.39,5.16-54.47A8,8,0,0,1,41.93,68c30.52-11.79,81.66-12,85.85-12h.27c.54,0,54.38-.18,86,12a8,8,0,0,1,4.77,5.49C221.2,82.61,224,99.43,224,128S221.2,173.39,218.84,182.47Z">
+                        </path>
+                    </svg>
+                </a>
+            </div>
+
+            <!-- Botão de Login (Visível em todas as telas) -->
+            <div class="d-none d-lg-flex custom-login-button align-items-center ms-3">
+                <a href="{{ url('/ibbb/login') }}" title="Login" name="btnlogin"
+                    class="btn btn-sm btn-primary shadow mt-1" role="button" data-bs-toggle="tooltip"
+                    data-bs-placement="top" aria-label="Fazer login no sistema">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#fff"
+                        viewBox="0 0 256 256">
+                        <path
+                            d="M230.92,212c-15.23-26.33-38.7-45.21-66.09-54.16a72,72,0,1,0-73.66,0C63.78,166.78,40.31,185.66,25.08,212a8,8,0,1,0,13.85,8c18.84-32.56,52.14-52,89.07-52s70.23,19.44,89.07,52a8,8,0,1,0,13.85-8ZM72,96a56,56,0,1,1,56,56A56.06,56.06,0,0,1,72,96Z">
+                        </path>
+                    </svg>
+                    <span class="ms-1">Login</span>
+                </a>
+            </div>
         </div>
     </div>
 </nav>
 
+<footer class="text-center py-3 bg-light fixed-bottom" role="contentinfo" aria-label="Rodapé">
+    <small>Direitos Reservados</small>
+</footer>
+
+
 <!-- Script para ajustar o texto do nome -->
-<script> 
+<script>
     function adjustNavbarText() {
         var navbarText = document.getElementById('navbarText');
         if (window.innerWidth <= 500) {
